8d595b92ff35c26c3c1cd880814fe2f8ae1abb32
vinaarchi <vinaa.as4@gmail.com>
2024-11-05 20:28:31 +0700

    message

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -5,36 +5,28 @@ import { Badge } from "./ui/badge";
 
 const Aboutme = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [hasDelayedScrolled, setHasDelayedScrolled] = useState(false);
 
   useEffect(() => {
     // Event listener untuk scroll
     const handleScroll = () => {
-      if (window.scrollY > 500) {
-        // gantiin 500 dengan treshold yang dimau
-        // Menambahkan delay sebelum mengubah state
-        setTimeout(() => {
-          setIsScrolled(true);
-          setHasDelayedScrolled(true); // Mengubah state setelah delay
-        }, 200); // Delay selama 200ms setelah scroll terdeteksi
+      if (window.scrollY > 100) {
+        // Ganti 100 dengan threshold yang diinginkan
+        setIsScrolled(true);
       } else {
         setIsScrolled(false);
-        setHasDelayedScrolled(false); // Reset state jika scroll kembali ke atas
       }
     };
 
-    // misalnya kalo di scroll, nnti handleScroll yang udah akan dipanggil lagi
+    // Menambahkan event listener untuk scroll
     window.addEventListener("scroll", handleScroll);
 
-    // untuk ngehapus event listener yang udah ditambahin sblm langkah pertamanya
-    // fungsinya untuk eventlistenernya tidak akan kepanggil lagi kalo komponen tidak dipakai
+    // Membersihkan event listener saat komponen unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
   return (
-    <div className="flex items-center pt-9 pb-9 px-10">
+    <div className="flex items-center pt-9 px-10">
       <Badge
         className={`flex justify-center items-center transition-all duration-500 ${
           isScrolled ? "bg-blue-300 rounded-xl p-6" : "h-1 w-full bg-blue-300"
@@ -43,9 +35,9 @@ const Aboutme = () => {
         <div className="text-center">
           <h1 className="font-rafeny text-5xl pb-6 font-light">About me</h1>
           <h3 className="text-lg font-helvetica font-light">
-            "I&apos;m a new web developer passionate about building responsive
-            and user-friendly websites. Continuously learning and eager to grow
-            in the tech world."
+            "I'm a new web developer passionate about building responsive and
+            user-friendly websites. Continuously learning and eager to grow in
+            the tech world."
           </h3>
         </div>
       </Badge>
