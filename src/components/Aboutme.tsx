429c28ee9c8ca63d88c20bd2f315673057e8aa20
vinaarchi <vinaa.as4@gmail.com>
2024-11-05 20:03:52 +0700

    message

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -10,7 +10,8 @@ const Aboutme = () => {
   useEffect(() => {
     // Event listener untuk scroll
     const handleScroll = () => {
-      if (window.scrollY > 500) { // gantiin 500 dengan treshold yang dimau
+      if (window.scrollY > 500) {
+        // gantiin 500 dengan treshold yang dimau
         // Menambahkan delay sebelum mengubah state
         setTimeout(() => {
           setIsScrolled(true);
@@ -32,25 +33,24 @@ const Aboutme = () => {
     };
   }, []);
 
-      return (
-        <div className="flex items-center pt-9 pb-9 px-10">
-          <Badge
-            className={`flex justify-center items-center transition-all duration-500 ${
-              isScrolled ? "bg-blue-300 rounded-xl p-6" : "h-1 w-full bg-blue-300"
-            }`}
-          >
-            <div className="text-center">
-              <h1 className="font-rafeny text-5xl pb-6 font-light">About me</h1>
-              <h3 className="text-lg font-helvetica font-light">
-                "I&quot;m a new web developer passionate about building responsive and
-                user-friendly websites. Continuously learning and eager to grow in the
-                tech world."
-              </h3>
-            </div>
-          </Badge>
+  return (
+    <div className="flex items-center pt-9 pb-9 px-10">
+      <Badge
+        className={`flex justify-center items-center transition-all duration-500 ${
+          isScrolled ? "bg-blue-300 rounded-xl p-6" : "h-1 w-full bg-blue-300"
+        }`}
+      >
+        <div className="text-center">
+          <h1 className="font-rafeny text-5xl pb-6 font-light">About me</h1>
+          <h3 className="text-lg font-helvetica font-light">
+            "I&apos;m a new web developer passionate about building responsive
+            and user-friendly websites. Continuously learning and eager to grow
+            in the tech world."
+          </h3>
         </div>
-      );
-    };
-    
-    export default Aboutme;
+      </Badge>
+    </div>
+  );
+};
 
+export default Aboutme;
